d1291fe9fbcf5b381f9fe5fe96e534949827208b
Sujeet Kumar <skumar450@gatech.edu>
2020-06-18 22:11:28 -0500

    First commit

---
+++ src/main/java/com/spring/springboot/demo/myCoolApp/controller/CoolAppRestController.java
@@ -0,0 +1,28 @@
+package com.spring.springboot.demo.myCoolApp.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoolAppRestController {
+
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo()
+	{
+		return "Coach name: "+coachName+" Team Name: "+teamName;
+	}
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "Hello World! Time on server is - "+LocalDateTime.now();
+	}
+}
---
+++ src/test/java/com/spring/springboot/demo/myCoolApp/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springboot.demo.myCoolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
